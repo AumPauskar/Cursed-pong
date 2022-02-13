584241833be05596ae6621069a65b74891507a69
Aum Pauskar <aumpauskar111@gmail.com>
2022-02-13 18:19:58 +0530

    config file and its dependencies added

---
+++ fileseeker.py
@@ -0,0 +1,11 @@
+def get_window_size():
+	import json
+	with open('config.json') as file:
+		data = json.load(file)
+		return (data['window-size-x'], data['window-size-y'])
+
+def get_window_title():
+	import json
+	with open('config.json') as file:
+		data = json.load(file)
+		return ['window-title']
