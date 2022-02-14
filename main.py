7d2ee80ae62c185c643cc7fd4bd4a7ee26e9c72c
Aum Pauskar <aumpauskar111@gmail.com>
2022-02-14 21:04:04 +0530

    Ball image added (with basic movement)

--- main.py
+++ main.py
@@ -31,8 +31,9 @@ charles_img = pygame.image.load('assets/images/player_16.png')
 #ball
 ball_x = 640
 ball_y = 360
-ball_delta_x = 0
-ball_delta_y = 0
+ball_delta_x = 2
+ball_delta_y = 2
+ball_img = pygame.image.load('assets/images/sbinotto.png')
 
 # background update
 def background_update():
@@ -40,10 +41,13 @@ def background_update():
 
 # player update
 def player_update_16(x, y):
-	screen.blit(charles_img, (x,y))
+	screen.blit(charles_img, (x, y))
 
 def player_update_55(x ,y):
-    screen.blit(carlos_img, (x,y))
+    screen.blit(carlos_img, (x, y))
+
+def ball_update(x, y):
+	screen.blit(ball_img, (x, y))
 
 
 while_key = True
@@ -52,6 +56,7 @@ while while_key == True:
 	background_update()
 	player_update_16(charles_x, charles_y)	
 	player_update_55(carlos_x, carlos_y)
+	ball_update(ball_x, ball_y)
 
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
@@ -73,7 +78,9 @@ while while_key == True:
 			if (event.key == pygame.K_w) or (event.key == pygame.K_s):
 				carlos_delta_y = 0
 
-
+	ball_x += ball_delta_x
+	ball_y += ball_delta_y
+	
 	if 0>charles_y:
 		charles_y = 0
 	elif 464<charles_y:
